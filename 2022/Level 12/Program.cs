3f0eb2a988eac035a1306a50c9d41a3a386b68ab
Philipp Punz-Reidlinger <philipp.punz@kcc.at>
2022-12-12 10:21:55 +0100

    2022 day 12

--- 2022/Level 12/Program.cs
+++ 2022/Level 12/Program.cs
@@ -5,36 +5,54 @@ var board = new Dictionary<Point, char>(
                 from x in Enumerable.Range(0, input[0].Length)
                 select new KeyValuePair<Point, char>(new Point(x, y), input[y][x]));
 
-var start = board.FirstOrDefault(d => d.Value == 'S').Key;
+List<Point> startPoints = new();
+startPoints = board.Where(d => d.Value == 'S' || d.Value == 'a').Select(d => d.Key).ToList();
+//startPoints = board.Where(d => d.Value == 'S').Select(d => d.Key).ToList();
 var endPoint = board.FirstOrDefault(d => d.Value == 'E').Key;
 
-var queue = new PriorityQueue<Point, int>();
-var resultBoard = new Dictionary<Point, int>();
-
-resultBoard[start] = 0;
-queue.Enqueue(start, 0);
-
-while (true)
+var results = new List<int>();
+foreach (var start in startPoints)
 {
-    var currentPoint = queue.Dequeue();
-    if (currentPoint == endPoint)
-    {
-        break;
-    }
+    var queue = new PriorityQueue<Point, int>();
+    var resultBoard = new Dictionary<Point, int>();
 
-    var neighbours = GetNeighbours(currentPoint).ToList();
-    foreach (var neighbour in neighbours)
+    resultBoard[start] = 0;
+    queue.Enqueue((start), 0);
+
+    while (true)
     {
-        var steps = resultBoard[currentPoint] + 1;
-        if (steps < resultBoard.GetValueOrDefault(neighbour))
+        Point currentPoint = null;
+        try
         {
-            resultBoard[neighbour] = steps;
-            queue.Enqueue(neighbour, steps);
+            currentPoint = queue.Dequeue();
+        }
+        catch
+        {
+            break;
+        }
+
+        if (currentPoint == endPoint)
+        {
+            results.Add(resultBoard[endPoint]);
+            break;
+        }
+
+        var neighbours = GetNeighbours(currentPoint).ToList();
+        foreach (var neighbour in neighbours)
+        {
+            var steps = resultBoard[currentPoint] + 1;
+            if (steps < resultBoard.GetValueOrDefault(neighbour, int.MaxValue))
+            {
+                resultBoard[neighbour] = steps;
+                queue.Enqueue(neighbour, steps);
+            }
         }
     }
 }
 
-Console.WriteLine(resultBoard[endPoint]);
+
+Console.WriteLine(results.Min());
+Console.ReadKey();
 
 IEnumerable<Point> GetNeighbours(Point currentPoint)
 {
@@ -47,7 +65,7 @@ IEnumerable<Point> GetNeighbours(Point currentPoint)
 
     return allowedNeighbours.Where(d =>
     {
-        if (board.TryGetValue(currentPoint, out var point) && board.TryGetValue(d, out var x) && (point + 1 == x || point == x || (point == 'S' && x == 'a') || (point == 'z' && x == 'E')))
+        if (board.TryGetValue(currentPoint, out var point) && board.TryGetValue(d, out var x) && (point + 1 == x || point == x || (point - 2 == x) || (point == 'S' && x == 'a') || (point == 'z' && x == 'E')))
         {
             return true;
         }
