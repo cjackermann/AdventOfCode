9cebf88a48f1715f8dc16fb6345fa94803a3014b
Philipp Punz-Reidlinger <philipp.punz@kcc.at>
2022-12-15 08:05:31 +0100

    2022 day 15

---
+++ 2022/Level 15/Program.cs
@@ -0,0 +1,61 @@
+﻿string[] input = File.ReadAllLines("input.txt");
+
+IEnumerable<(Point Sensor, Point Beacon)> data = from line in input
+                                                 let parts = line.Split(new string[] { "Sensor at x=", ", y=", ": closest beacon is at x=", ", y=" }, StringSplitOptions.RemoveEmptyEntries)
+                                                 let sensor = new Point(int.Parse(parts[0]), int.Parse(parts[1]))
+                                                 let beacon = new Point(int.Parse(parts[2]), int.Parse(parts[3]))
+                                                 select (sensor, beacon);
+
+HashSet<Point> empty = new();
+HashSet<Point> sensors = new();
+HashSet<Point> beacons = new();
+foreach (var pair in data)
+{
+    sensors.Add(pair.Sensor);
+    beacons.Add(pair.Beacon);
+
+    var xDiff = Math.Abs(pair.Sensor.X - pair.Beacon.X);
+    var yDiff = Math.Abs(pair.Sensor.Y - pair.Beacon.Y);
+    var together = xDiff + yDiff;
+
+    var leftPoint = new Point(pair.Sensor.X - together, pair.Sensor.Y);
+    var rightPoint = new Point(pair.Sensor.X + together, pair.Sensor.Y);
+    var downPoint = new Point(pair.Sensor.X, pair.Sensor.Y + together);
+    var upPoint = new Point(pair.Sensor.X, pair.Sensor.Y - together);
+
+    int step = 0;
+    for (int y = pair.Sensor.Y; y <= pair.Sensor.Y + together; y++)
+    {
+        if (y == 2000000)
+        {
+            for (int x = pair.Sensor.X - together + step; x <= pair.Sensor.X + together - step; x++)
+            {
+                empty.Add(new Point(x, y));
+            }
+        }
+
+        step++;
+    }
+
+    step = 0;
+    for (int y = pair.Sensor.Y; y >= pair.Sensor.Y - together; y--)
+    {
+        if (y == 2000000)
+        {
+            for (int x = pair.Sensor.X - together + step; x <= pair.Sensor.X + together - step; x++)
+            {
+                empty.Add(new Point(x, y));
+            }
+        }
+
+        step++;
+    }
+}
+
+var all = empty.Union(sensors);
+var test = all.Where(d => d.Y == 2000000).ToHashSet();
+
+Console.WriteLine(test.Count - 1);
+Console.ReadKey();
+
+public record Point(int X, int Y);
