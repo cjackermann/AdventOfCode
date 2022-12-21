bce995b25aff4b7af43c0f870535795074724a7d
Philipp Punz-Reidlinger <philipp.punz@kcc.at>
2022-12-21 07:24:58 +0100

    day 19 unfinished

---
+++ 2022/Level 19/Program.cs
@@ -0,0 +1,79 @@
+﻿using Level_19;
+
+string[] input = File.ReadAllLines("input.txt");
+
+var blueprints = from line in input
+                 let parts = line.Split(new string[] { "Blueprint ", ": Each ore robot costs ", " ore. Each clay robot costs ", " ore. Each obsidian robot costs ", " ore and ", " clay. Each geode robot costs ", " ore and ", " obsidian." }, StringSplitOptions.RemoveEmptyEntries)
+                 let id = int.Parse(parts[0])
+                 let oreRobotCost = int.Parse(parts[1])
+                 let clayRobotCost = int.Parse(parts[2])
+                 let obsidianRobotCost = (int.Parse(parts[3]), int.Parse(parts[4]))
+                 let geodeRobotCost = (int.Parse(parts[5]), int.Parse(parts[6]))
+                 select new Blueprint(id, oreRobotCost, clayRobotCost, obsidianRobotCost, geodeRobotCost);
+
+int result = 0;
+foreach (var blueprint in blueprints.Take(1))
+{
+    result += blueprint.Id * Calculate(blueprint);
+}
+
+Console.WriteLine(result);
+Console.ReadKey();
+
+static int Calculate(Blueprint blueprint)
+{
+    List<Robot> activeRobots = new() { new Robot(RobotType.Ore) };
+
+    var previousRounds = new HashSet<Round>();
+    var rounds = new HashSet<Round> { new Round(0, 1, 0, 0, 0, 0, 0, 0, 24) };
+    int geodeCount = 0;
+
+    for (int minute = 1; minute <= 24; minute++)
+    {
+        var newRobots = BuildNewRobots().ToList();
+
+        oreCount += activeRobots.Count(d => d.Type == RobotType.Ore);
+        clayCount += activeRobots.Count(d => d.Type == RobotType.Clay);
+        obsidianCount += activeRobots.Count(d => d.Type == RobotType.Obsidian);
+        geodeCount += activeRobots.Count(d => d.Type == RobotType.Geode);
+
+        activeRobots.AddRange(newRobots);
+    }
+
+    return geodeCount;
+
+    IEnumerable<Robot> BuildNewRobots()
+    {
+        if (oreCount >= blueprint.GeodeRobotCost.Ore && obsidianCount >= blueprint.GeodeRobotCost.Obsidian)
+        {
+            yield return new Robot(RobotType.Geode);
+            oreCount -= blueprint.GeodeRobotCost.Ore;
+            obsidianCount -= blueprint.GeodeRobotCost.Obsidian;
+        }
+
+        if (oreCount >= blueprint.ObsidianRobotCost.Ore && clayCount >= blueprint.ObsidianRobotCost.Clay)
+        {
+            yield return new Robot(RobotType.Obsidian);
+            oreCount -= blueprint.ObsidianRobotCost.Ore;
+            clayCount -= blueprint.ObsidianRobotCost.Clay;
+        }
+
+        if (oreCount >= blueprint.ClayRobotCost)
+        {
+            yield return new Robot(RobotType.Clay);
+            oreCount -= blueprint.ClayRobotCost;
+        }
+    }
+}
+
+public record Blueprint(int Id, int OreRobotCost, int ClayRobotCost, (int Ore, int Clay) ObsidianRobotCost, (int Ore, int Obsidian) GeodeRobotCost);
+
+public record Robot(RobotType Type);
+
+public enum RobotType
+{
+    Ore,
+    Clay,
+    Obsidian,
+    Geode,
+}
---
+++ 2022/Level 19/Round.cs
@@ -0,0 +1,63 @@
+﻿namespace Level_19
+{
+    public class Round
+    {
+        public Round(int oreCount, int oreRobotsCount, int clayCount, int clayRobotsCount, int obsidianCount, int obsidianRobotsCount, int geodeCount, int geodeRobotsCount, int time)
+        {
+            OreCount = oreCount;
+            OreRobotsCount = oreRobotsCount;
+            ClayCount = clayCount;
+            ClayRobotsCount = clayRobotsCount;
+            ObsidianCount = obsidianCount;
+            ObsidianRobotsCount = obsidianRobotsCount;
+            GeodeCount = geodeCount;
+            GeodeRobotsCount = geodeRobotsCount;
+            Time = time;
+        }
+
+        public int OreCount { get; }
+
+        public int OreRobotsCount { get; }
+
+        public int ClayCount { get; }
+
+        public int ClayRobotsCount { get; }
+
+        public int ObsidianCount { get; }
+
+        public int ObsidianRobotsCount { get; }
+
+        public int GeodeCount { get; }
+
+        public int GeodeRobotsCount { get; }
+
+        public int Time { get; }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 187;
+                hash = (187 * hash) + OreCount;
+                hash = (187 * hash) + OreRobotsCount;
+                hash = (187 * hash) + ClayCount;
+                hash = (187 * hash) + ClayRobotsCount;
+                hash = (187 * hash) + ObsidianCount;
+                hash = (187 * hash) + ObsidianRobotsCount;
+                hash = (187 * hash) + GeodeCount;
+                hash = (187 * hash) + GeodeRobotsCount;
+                return hash;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is not Round other)
+            {
+                return false;
+            }
+
+            return OreCount == other.OreCount && OreRobotsCount == other.OreRobotsCount && ClayCount == other.ClayCount && ClayRobotsCount == other.ClayRobotsCount && ObsidianCount == other.ObsidianCount && ObsidianRobotsCount == other.ObsidianRobotsCount && GeodeCount == other.GeodeCount && GeodeRobotsCount == other.GeodeRobotsCount && Time == other.Time;
+        }
+    }
+}
