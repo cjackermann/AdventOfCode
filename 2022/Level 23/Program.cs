b64adbd4ff55ae7d1933c6dc8835ff88844cda39
Philipp Punz-Reidlinger <philipp.punz@kcc.at>
2022-12-23 08:29:54 +0100

    2022 day 23 part one

---
+++ 2022/Level 23/Program.cs
@@ -0,0 +1,120 @@
+﻿string[] input = File.ReadAllLines("input.txt");
+
+HashSet<Point> elfes = new();
+for (int y = 0; y < input.Length; y++)
+{
+    for (int x = 0; x < input[0].Length; x++)
+    {
+        if (input[y][x] == '#')
+        {
+            elfes.Add(new Point(x, y));
+        }
+    }
+}
+
+var result = PartOne(elfes);
+
+int xMin = result.Min(d => d.X);
+int xMax = result.Max(d => d.X) + 1;
+int yMin = result.Min(d => d.Y);
+int yMax = result.Max(d => d.Y) + 1;
+
+Console.WriteLine("Stage 1: " + ((xMax - xMin) * (yMax - yMin) - result.Count));
+Console.ReadKey();
+
+HashSet<Point> PartOne(HashSet<Point> elfes)
+{
+    Direction direction = Direction.North;
+    for (int i = 0; i < 10; i++)
+    {
+        var possibleNextPositions = new Dictionary<Point, Point>();
+        foreach (var elfe in elfes)
+        {
+            CheckNextMove(direction, elfes, elfe, possibleNextPositions);
+        }
+
+        var singlePoints = possibleNextPositions.GroupBy(d => d.Value).Where(d => d.Count() == 1).ToDictionary(d => d.First().Key, d => d.Key);
+        foreach (var proposal in singlePoints)
+        {
+            elfes.Remove(proposal.Key);
+            elfes.Add(proposal.Value);
+        }
+
+        direction = direction == Direction.East ? Direction.North : direction + 1;
+    }
+
+    return elfes;
+}
+
+void CheckNextMove(Direction direction, HashSet<Point> elfes, Point elfe, Dictionary<Point, Point> possibleNextPositions)
+{
+    var neighbours = GetNeighbours(elfe, elfes);
+    if (!neighbours.Any())
+    {
+        return;
+    }
+
+    Point nextPosition = null;
+    for (int move = (int)direction; move < (int)direction + 4; move++)
+    {
+        if (move % 4 == 0 && !neighbours.Any(d => d == Direction.North))
+        {
+            nextPosition = elfe with { Y = elfe.Y - 1 };
+        }
+        else if (move % 4 == 1 && !neighbours.Any(d => d == Direction.South))
+        {
+            nextPosition = elfe with { Y = elfe.Y + 1 };
+        }
+        else if (move % 4 == 2 && !neighbours.Any(d => d == Direction.West))
+        {
+            nextPosition = elfe with { X = elfe.X - 1 };
+        }
+        else if (move % 4 == 3 && !neighbours.Any(d => d == Direction.East))
+        {
+            nextPosition = elfe with { X = elfe.X + 1 };
+        }
+
+        if (nextPosition != null)
+        {
+            break;
+        }
+    }
+
+    if (nextPosition != null)
+    {
+        possibleNextPositions.Add(elfe, nextPosition);
+    }
+}
+
+static IEnumerable<Direction> GetNeighbours(Point elve, HashSet<Point> elfes)
+{
+    if (elfes.Any(d => d.X == elve.X && d.Y == elve.Y - 1) || elfes.Any(d => d.X == elve.X + 1 && d.Y == elve.Y - 1) || elfes.Any(d => d.X == elve.X - 1 && d.Y == elve.Y - 1))
+    {
+        yield return Direction.North;
+    }
+
+    if (elfes.Any(d => d.X == elve.X && d.Y == elve.Y + 1) || elfes.Any(d => d.X == elve.X + 1 && d.Y == elve.Y + 1) || elfes.Any(d => d.X == elve.X - 1 && d.Y == elve.Y + 1))
+    {
+        yield return Direction.South;
+    }
+
+    if (elfes.Any(d => d.X == elve.X - 1 && d.Y == elve.Y) || elfes.Any(d => d.X == elve.X - 1 && d.Y == elve.Y + 1) || elfes.Any(d => d.X == elve.X - 1 && d.Y == elve.Y - 1))
+    {
+        yield return Direction.West;
+    }
+
+    if (elfes.Any(d => d.X == elve.X + 1 && d.Y == elve.Y) || elfes.Any(d => d.X == elve.X + 1 && d.Y == elve.Y + 1) || elfes.Any(d => d.X == elve.X + 1 && d.Y == elve.Y - 1))
+    {
+        yield return Direction.East;
+    }
+}
+
+public record Point(int X, int Y);
+
+public enum Direction
+{
+    North = 0,
+    South = 1,
+    West = 2,
+    East = 3,
+}
