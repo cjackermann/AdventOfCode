7f190e95349bc26bb7ffa8bf359535a60fd1ad50
Philipp Punz-Reidlinger <philipp.punz@kcc.at>
2023-12-04 07:55:27 +0100

    2023 day 4 part 2

--- 2023/Level 04/Program.cs
+++ 2023/Level 04/Program.cs
@@ -1,6 +1,7 @@
 ﻿string[] input = File.ReadAllLines("input.txt");
 
 PartOne(input);
+PartTwo(input);
 
 Console.ReadKey();
 
@@ -21,4 +22,44 @@ void PartOne(string[] input)
     }
 
     Console.WriteLine("Part 1: " + counter);
+}
+
+void PartTwo(string[] input)
+{
+    var games = (from line in input
+                 let blocks = line.Split(new char[] { ':', '|' })
+                 let left = blocks[1].Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 let right = blocks[2].Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 select new { left, right }).ToList();
+
+    long counter = 0;
+    Dictionary<int, long> dict = new Dictionary<int, long>();
+
+    for (int i = 0; i < games.Count; i++)
+    {
+        var winningNumbers = games[i].left.Intersect(games[i].right).Count();
+        long gameCount = 1;
+
+        if (dict.TryGetValue(i, out long value))
+        {
+            gameCount += value;
+            dict.Remove(i);
+        }
+
+        for (int x = 1; x <= winningNumbers; x++)
+        {
+            if (dict.ContainsKey(i + x))
+            {
+                dict[i + x] += gameCount;
+            }
+            else
+            {
+                dict[i + x] = gameCount;
+            }
+        }
+
+        counter += gameCount;
+    }
+
+    Console.WriteLine("Part 2: " + counter);
 }
