ca7748abfdecbaf0ad3ddc541dc70a2e3bd0c009
Philipp Punz-Reidlinger <philipp.punz@kcc.at>
2023-12-10 09:08:48 +0100

    2023 day 10 part 2

--- 2023/Level 10/Program.cs
+++ 2023/Level 10/Program.cs
@@ -5,6 +5,9 @@ var board = new Dictionary<Point, char>(
                 from x in Enumerable.Range(0, input[0].Length)
                 select new KeyValuePair<Point, char>(new Point(x, y), input[y][x]));
 
+int maxX = board.Max(x => x.Key.X);
+int maxY = board.Max(x => x.Key.Y);
+
 var start = board.First(x => x.Value == 'S').Key;
 PriorityQueue<Point, int> queue = new();
 Dictionary<Point, int> resultBoard = new() { [start] = 0 };
@@ -35,6 +38,35 @@ while (true)
 }
 
 Console.WriteLine("Part 1: " + resultBoard.Max(x => x.Value));
+
+foreach (var point in board.Keys.Where(x => !resultBoard.ContainsKey(x)))
+{
+    board[point] = '.';
+}
+
+long counter = 0;
+for (int y = 0; y < maxY; y++)
+{
+    bool inside = false;
+    char value = board[new Point(0, y)];
+
+    for (int x = 0; x < maxX; x++)
+    {
+        char currentValue = board[new Point(x, y)];
+
+        switch (currentValue)
+        {
+            case '|': inside = !inside; break;
+            case 'F': value = 'F'; break;
+            case 'L': value = 'L'; break;
+            case '7': if (value == 'L') inside = !inside; break;
+            case 'J': if (value == 'F') inside = !inside; break;
+            case '.': if (inside) counter++; break;
+        }
+    }
+}
+
+Console.WriteLine("Part 2: " + counter);
 Console.ReadKey();
 
 IEnumerable<Point> GetNeighbours(Point currentPoint)
