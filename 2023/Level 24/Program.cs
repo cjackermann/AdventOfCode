c0220ccc3d8e34382edf41313040f078ab6abf1c
Philipp Punz <philipp.punz@kcc.at>
2023-12-24 11:54:01 +0100

    2023 day 24 part 2

--- 2023/Level 24/Program.cs
+++ 2023/Level 24/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Level_24;
+using Microsoft.Z3;
 
 string[] lines = File.ReadAllLines("input.txt");
 
@@ -70,4 +71,61 @@ foreach (var hailstoneA in hailstones)
 }
 
 Console.WriteLine("Part 1: " + intersections);
+Console.WriteLine("Part 2: " + Solve(hailstones));
+
 Console.ReadKey();
+
+// using microsoft z3 nuget package - see: https://github.com/Z3Prover/z3
+static long Solve(List<Hailstone> hailstones) 
+{
+    var ctx = new Context();
+    var solver = ctx.MkSolver();
+
+    // Coordinates of the stone
+    var x = ctx.MkIntConst("x");
+    var y = ctx.MkIntConst("y");
+    var z = ctx.MkIntConst("z");
+
+    // Velocity of the stone
+    var vx = ctx.MkIntConst("vx");
+    var vy = ctx.MkIntConst("vy");
+    var vz = ctx.MkIntConst("vz");
+
+    // For each iteration, we will add 3 new equations to the solver.
+    // We want to find 9 variables (x, y, z, vx, vy, vz, t0, t1, t2) that satisfy all the equations, so a system of 9 equations is enough.
+    for (var i = 0; i < 3; i++)
+    {
+        var t = ctx.MkIntConst($"t{i}");
+        var hailstone = hailstones[i];
+
+        var px = ctx.MkInt(Convert.ToInt64(hailstone.Position.X));
+        var py = ctx.MkInt(Convert.ToInt64(hailstone.Position.Y));
+        var pz = ctx.MkInt(Convert.ToInt64(hailstone.Position.Z));
+
+        var pvx = ctx.MkInt(Convert.ToInt64(hailstone.Velocity.X));
+        var pvy = ctx.MkInt(Convert.ToInt64(hailstone.Velocity.Y));
+        var pvz = ctx.MkInt(Convert.ToInt64(hailstone.Velocity.Z));
+
+        var xLeft = ctx.MkAdd(x, ctx.MkMul(t, vx)); // x + t * vx
+        var yLeft = ctx.MkAdd(y, ctx.MkMul(t, vy)); // y + t * vy
+        var zLeft = ctx.MkAdd(z, ctx.MkMul(t, vz)); // z + t * vz
+
+        var xRight = ctx.MkAdd(px, ctx.MkMul(t, pvx)); // px + t * pvx
+        var yRight = ctx.MkAdd(py, ctx.MkMul(t, pvy)); // py + t * pvy
+        var zRight = ctx.MkAdd(pz, ctx.MkMul(t, pvz)); // pz + t * pvz
+
+        solver.Add(t >= 0); // time should always be positive - we don't want solutions for negative time
+        solver.Add(ctx.MkEq(xLeft, xRight)); // x + t * vx = px + t * pvx
+        solver.Add(ctx.MkEq(yLeft, yRight)); // y + t * vy = py + t * pvy
+        solver.Add(ctx.MkEq(zLeft, zRight)); // z + t * vz = pz + t * pvz
+    }
+
+    solver.Check();
+    var model = solver.Model;
+
+    var rx = model.Eval(x);
+    var ry = model.Eval(y);
+    var rz = model.Eval(z);
+
+    return long.Parse(rx.ToString()) + long.Parse(ry.ToString()) + long.Parse(rz.ToString());
+}
