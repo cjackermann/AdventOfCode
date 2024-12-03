0ce03ebd8635c23bfd64813acc207b60b21d3436
Philipp Punz-Reidlinger <philipp.punz@kcc.at>
2024-12-03 06:44:06 +0100

    2024 day 3

---
+++ 2024/Level 03/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.RegularExpressions;
+
+string input = File.ReadAllText("input.txt");
+
+Part1(input);
+Part2(input);
+
+Console.ReadKey();
+
+static void Part1(string input)
+{
+    string pattern = @"mul\((\d+),(\d+)\)";
+    var matches = Regex.Matches(input, pattern);
+
+    long result = 0;
+    foreach (Match match in matches)
+    {
+        var x = long.Parse(match.Groups[1].Value);
+        var y = long.Parse(match.Groups[2].Value);
+
+        result += x * y;
+    }
+
+    Console.WriteLine("Part1: " + result);
+}
+
+static void Part2(string input)
+{
+    string pattern = @"(do\(\)|don't\(\))|mul\((\d+),(\d+)\)";
+    var matches = Regex.Matches(input, pattern);
+
+    long result = 0;
+    bool increaseResult = true;
+    for (int i = 0; i < matches.Count; i++)
+    {
+        Match match = matches[i];
+
+        if (match.Value == "don't()")
+        {
+            increaseResult = false;
+            continue;
+        }
+        else if (match.Value == "do()")
+        {
+            increaseResult = true;
+            continue;
+        }
+        else if (increaseResult)
+        {
+            var x = long.Parse(match.Groups[2].Value);
+            var y = long.Parse(match.Groups[3].Value);
+
+            result += x * y;
+        }
+    }
+
+    Console.WriteLine("Part2: " + result);
+}
