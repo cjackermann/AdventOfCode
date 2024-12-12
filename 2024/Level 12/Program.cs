0ba24f24fbcc66fd5e062e8531c1e60d3c2fd8cf
Philipp Punz-Reidlinger <philipp.punz@kcc.at>
2024-12-12 12:54:23 +0100

    2024 day 12 full

--- 2024/Level 12/Program.cs
+++ 2024/Level 12/Program.cs
@@ -7,26 +7,40 @@ var board = new Dictionary<Point, string>(
                 from x in Enumerable.Range(0, input[0].Length)
                 select new KeyValuePair<Point, string>(new Point(x, y), input[y][x].ToString()));
 
-long result = 0;
-HashSet<Point> checkedPoints = [];
+long result1 = 0;
+long result2 = 0;
+HashSet<Point> checkedPoints1 = [];
+HashSet<Point> checkedPoints2 = [];
 foreach (var point in board)
 {
-    if (checkedPoints.Contains(point.Key))
+    if (checkedPoints1.Contains(point.Key))
     {
         continue;
     }
 
+    // Part1
     var tmpResult = CalculateForPoint(point.Key, point.Value);
+    result1 += tmpResult.Item1 * tmpResult.Item2;
 
-    result += tmpResult.Item1 * tmpResult.Item2;
+    // Part2
+    var points = GetPoints(point.Key, point.Value).ToList();
+    int sides = 0;
+
+    sides += GetYPoints(points.Select(s => s with { Y = s.Y - 1}).Except(points).GroupBy(s => s.Y));
+    sides += GetYPoints(points.Select(s => s with { Y = s.Y + 1}).Except(points).GroupBy(s => s.Y));
+    sides += GetXPoints(points.Select(s => s with { X = s.X - 1}).Except(points).GroupBy(s => s.X));
+    sides += GetXPoints(points.Select(s => s with { X = s.X + 1}).Except(points).GroupBy(s => s.X));
+
+    result2 += points.Count * sides;
 }
 
-Console.WriteLine("Part 1: " + result);
+Console.WriteLine("Part 1: " + result1);
+Console.WriteLine("Part 2: " + result2);
 Console.ReadKey();
 
 (int, int) CalculateForPoint(Point currentPoint, string currentValue)
 {
-    checkedPoints.Add(currentPoint);
+    checkedPoints1.Add(currentPoint);
 
     Point[] neighbours =
     [
@@ -42,7 +56,7 @@ Console.ReadKey();
     int count = 1;
     foreach (var neighbour in valueNeighbours)
     {
-        if (checkedPoints.Contains(neighbour))
+        if (checkedPoints1.Contains(neighbour))
         {
             continue;
         }
@@ -53,4 +67,74 @@ Console.ReadKey();
     }
 
     return (count, bounds);
+}
+
+IEnumerable<Point> GetPoints(Point currentPoint, string currentValue)
+{
+    checkedPoints2.Add(currentPoint);
+
+    Point[] neighbours =
+    [
+        new Point(currentPoint.X + 1, currentPoint.Y),
+        new Point(currentPoint.X - 1, currentPoint.Y),
+        new Point(currentPoint.X, currentPoint.Y + 1),
+        new Point(currentPoint.X, currentPoint.Y - 1),
+    ];
+
+    var valueNeighbours = neighbours.Where(x => board.TryGetValue(x, out var value) && value == currentValue).ToList();
+
+    foreach (var neighbour in valueNeighbours)
+    {
+        if (checkedPoints2.Contains(neighbour))
+        {
+            continue;
+        }
+
+        foreach (var point in GetPoints(neighbour, currentValue))
+        {
+            yield return point;
+        }
+    }
+
+    yield return currentPoint;
+}
+
+int GetYPoints(IEnumerable<IGrouping<int, Point>> groups)
+{
+    var result = 0;
+    foreach (var group in groups)
+    {
+        result++;
+
+        var orderedList = group.OrderBy(s => s.X).ToList();
+        for (int i = 0; i < orderedList.Count - 1; i++)
+        {
+            if (orderedList[i].X + 1 != orderedList[i + 1].X)
+            {
+                result++;
+            }
+        }
+    }
+
+    return result;
+}
+
+int GetXPoints(IEnumerable<IGrouping<int, Point>> groups)
+{
+    var result = 0;
+    foreach (var group in groups)
+    {
+        result++;
+
+        var orderedList = group.OrderBy(s => s.Y).ToList();
+        for (int i = 0; i < orderedList.Count - 1; i++)
+        {
+            if (orderedList[i].Y + 1 != orderedList[i + 1].Y)
+            {
+                result++;
+            }
+        }
+    }
+
+    return result;
 }
